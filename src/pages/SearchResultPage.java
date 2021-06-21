22d70387f0728a8191080108358fcc2212e39504
MajaDramicanin <maja.dramicanin@gmail.com>
2021-06-21 21:43:01 +0200

    Search page and test

--- src/pages/SearchResultPage.java
+++ src/pages/SearchResultPage.java
@@ -1,13 +1,25 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchResultPage extends BasicPage {
 
 	public SearchResultPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
 		super(driver, waiter, js);
+	}
+	
+	public List<WebElement> getSearchResults(){
+		return driver.findElements(By.xpath("//*[@class=\"product-name\"]/a"));		
+	}
+	
+	public int getSearchResultsNumber() {
+		return this.getSearchResults().size();
 	}
 
 }
--- src/tests/SearchTest.java
+++ src/tests/SearchTest.java
@@ -1,5 +1,95 @@
 package tests;
 
-public class SearchTest extends BasicTest {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.LocationPopupPage;
+
+public class SearchTest extends BasicTest {
+	String url = this.baseUrl + "/meals";
+	String locationName = "City Center - Albany";
+
+	@Test
+	public void searchResultTest() throws InterruptedException, IOException {
+
+		SoftAssert sa = new SoftAssert();
+
+		File file = new File("data/Data.xlsx");
+		FileInputStream fis = new FileInputStream(file);
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("Meal Search Results");
+
+		driver.get(url);
+		
+		Thread.sleep(3000);
+		
+		locationPopupPage.setLocation(locationName);
+		
+		Thread.sleep(3000);
+
+		for (int i = 1; i <= 6; i++) {
+			String searchUrl = sheet.getRow(i).getCell(1).getStringCellValue();
+			String searchLocation = sheet.getRow(i).getCell(0).getStringCellValue();
+			int numberOfResults = (int) sheet.getRow(i).getCell(2).getNumericCellValue();
+			
+			System.out.println(searchUrl);
+			System.out.println(searchLocation);
+			System.out.println(numberOfResults);
+			
+			driver.get(searchUrl);
+			
+			Thread.sleep(3000);
+			
+			locationPopupPage.getLocationHeader().click();
+			
+			Thread.sleep(3000);
+			
+			locationPopupPage.setLocation(searchLocation);
+			
+			Thread.sleep(3000);
+			
+			int pageResNum = searchResultPage.getSearchResultsNumber();
+		
+			
+			System.out.println(pageResNum);
+			
+			sa.assertTrue(pageResNum == numberOfResults,
+					" [ERROR] Unexpected number of search results message ");
+			
+			Thread.sleep(3000);
+
+			for (int j = 0; j < numberOfResults; j++) {
+				
+				WebElement mealItem = searchResultPage.getSearchResults().get(j);
+				String meal = mealItem.getText();
+				
+				Thread.sleep(3000);
+				String searchMeal = sheet.getRow(i).getCell(j + 3).getStringCellValue();
+				
+				System.out.println(meal +"   "+ searchMeal);
+				
+				Thread.sleep(2000);
+				
+				sa.assertTrue(meal.contains(searchMeal), "[ERROR] Results don't match! ");
+				Thread.sleep(2000);
+			}
+
+			wb.close();
+			sa.assertAll();
+
+			Thread.sleep(1000);
+
+		}
+
+	}
 }
